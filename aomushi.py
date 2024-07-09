a2b4ffdf077402658870e54ec1f815b01e0c3b0b
unknown <c0b231461d@edu.teu.ac.jp>
2024-07-09 17:13:43 +0900

    移動処理の訂正 バグの修正

--- aomushi.py
+++ aomushi.py
@@ -10,24 +10,44 @@ HEIGHT = 500  # ゲームウィンドウの高さ
 SIZE = 20
 
 class Snake:
+    """
+    メインで動かす青虫の定義
+    """
     def __init__(self):
+        """
+        特定の変数に青虫の情報を入れる
+        引数なし：
+        """
         self.body = deque([(WIDTH//2, HEIGHT//2), (WIDTH//2-SIZE, HEIGHT//2), (WIDTH//2-2*SIZE, HEIGHT//2)])
         self.direction = (SIZE, 0)  # 初期の移動方向は右
         self.move = False  # 蛇が動くかどうかを示すフラグ
         self.body_image = pg.image.load("fig/body_L.png")  # 蛇の体の画像を読み込む
 
     def update(self):
+        """
+        青虫の座標の更新
+        引数なし：
+        """
         if self.move:  # moveがTrueのときだけ更新する
             # 新しいヘッドを作成
             new_head = (self.body[0][0] + self.direction[0], self.body[0][1] + self.direction[1])
             # 体を更新
             self.body.appendleft(new_head)
             self.body.pop()
+            self.move = False  # 連打で一マスずつ動くようになります
 
-    def change_direction(self, direction):
+    def change_direction(self, direction:tuple):
+        """
+        青虫の移動する距離の設定
+        引数１：移動する距離の座標tuple型
+        """
         self.direction = direction
 
-    def draw(self, screen):
+    def draw(self, screen:pg):
+        """
+        描画の更新
+        引数:screenディスプレイの情報
+        """
         for segment in self.body:
             screen.blit(self.body_image, segment)  # 各セグメントに画像を描画
 
@@ -54,8 +74,10 @@ def main():
             elif event.type == pg.KEYUP:
                 if event.key in [pg.K_w, pg.K_a, pg.K_s, pg.K_d]:
                     snake.move = False  # キーが離されたら停止
+            if move_direction:  # breakで高速処理
+                break
 
-        if move_direction:
+        if move_direction :
             snake.change_direction(move_direction)
             snake.move = True  # キーが押されている間は移動
 
