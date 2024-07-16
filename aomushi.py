48f613438ac9f0f93861126ba60edfe3de935155
egami <c0a2302451@edu.tue.ac.jp>
2024-07-16 13:30:05 +0900

    基盤追加

--- aomushi.py
+++ aomushi.py
@@ -9,27 +9,63 @@ WIDTH = 500  # ゲームウィンドウの幅
 HEIGHT = 500  # ゲームウィンドウの高さ
 SIZE = 20
 
-class Insect:
+class Insect(pg.sprite.Sprite):
     """
     青虫に関するクラス
     """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_w: (0, -SIZE),
+        pg.K_s: (0, SIZE),
+        pg.K_a: (-SIZE, 0),
+        pg.K_d: (SIZE, 0),        
+    }
+    jump_height = -25  # ジャンプの高さ
+    gravity = 5  # 重力の強さ
+    
     def __init__(self):
-        self.body = deque([(WIDTH//2, HEIGHT-50), (WIDTH//2-SIZE, HEIGHT-50), (WIDTH//2-2*SIZE, HEIGHT-50)])
-        self.direction = (SIZE, 0)  # 初期の移動方向は右
-        self.move = False  # 虫が動くかどうかを示すフラグ
+        super().__init__()
+        self.body = deque([(WIDTH//2, HEIGHT-50), (WIDTH//2-SIZE, HEIGHT-50), (WIDTH//2-2*SIZE, HEIGHT-50)])  # 初期青虫
+        self.direction = (0, 0)  # 初期の移動方向
         self.body_image = pg.image.load("fig/body_L.png")  # 虫の体の画像を読み込む
+        self.jump_velocity = 0  # ジャンプの速度
+        self.is_jumping = False  # ジャンプ中かどうかのフラグ
+        self.start_y = HEIGHT - 50  # ジャンプ開始時の初期位置
+        self.rect = pg.Rect(self.body[0], (SIZE, SIZE))  # 初期の位置とサイズで矩形を作成
 
-    def update(self):
-        if self.move:  # moveがTrueのときだけ更新する
-            # 新しいヘッドを作成
-            new_head = (self.body[0][0] + self.direction[0], self.body[0][1] + self.direction[1])
-            # 体を更新
-            self.body.appendleft(new_head)
-            self.body.pop()
+    def update(self, key_lst: list[bool]):
+        sum_mv = [0, 0]
+        move_keys = [k for k in __class__.delta if key_lst[k]]
 
-    def change_direction(self, direction):
-        self.direction = direction
-
+        if len(move_keys) == 1:  # 1つの方向キーが押されている場合のみ移動
+            k = move_keys[0]
+            sum_mv[0] += __class__.delta[k][0]
+            sum_mv[1] += __class__.delta[k][1]
+        
+        if key_lst[pg.K_SPACE] and not self.is_jumping:
+            self.jump_velocity = __class__.jump_height
+            self.is_jumping = True
+            self.start_y = self.body[0][1]  # ジャンプ開始時の初期位置を記憶
+        
+        if self.is_jumping:
+            self.jump_velocity += __class__.gravity
+            sum_mv[1] += self.jump_velocity
+        
+        # 新しいヘッドを作成
+        new_head = (self.body[0][0] + sum_mv[0], self.body[0][1] + sum_mv[1])
+        
+        # ジャンプが元の位置に戻ったらジャンプを止める
+        if self.is_jumping and new_head[1] >= self.start_y:
+            new_head = (new_head[0], self.start_y)
+            self.is_jumping = False
+            self.jump_velocity = 0
+        
+        # 体を更新
+        self.body.appendleft(new_head)
+        self.body.pop()
+        
+        # 矩形の位置を更新
+        self.rect.topleft = self.body[0]
+        
     def draw(self, screen):
         for segment in self.body:
             screen.blit(self.body_image, segment)  # 各セグメントに画像を描画
@@ -41,30 +77,15 @@ def main():
     insect = Insect()
 
     while True:
-        move_direction = None  # キーが押されている場合の移動方向
+        key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    move_direction = (0, -SIZE)
-                elif event.key == pg.K_a:
-                    move_direction = (-SIZE, 0)
-                elif event.key == pg.K_s:
-                    move_direction = (0, SIZE)
-                elif event.key == pg.K_d:
-                    move_direction = (SIZE, 0)
-            elif event.type == pg.KEYUP:
-                if event.key in [pg.K_w, pg.K_a, pg.K_s, pg.K_d]:
-                    insect.move = False  # キーが離されたら停止
-
-        if move_direction:
-            insect.change_direction(move_direction)
-            insect.move = True  # キーが押されている間は移動
-
-        insect.update()
+            
         screen.blit(img, [0, 0])
-        insect.draw(screen)
+        insect.update(key_lst)  # 青虫
+        insect.draw(screen)  # 青虫
+
         pg.display.update()
         clock.tick(50)
 
